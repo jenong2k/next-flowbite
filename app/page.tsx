c70a48c454b8efdee5d0387a33d2fade92e72b39
Rudi <jenong@gmail.com>
2022-11-13 15:13:38 +0700

    flowbite setup

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,29 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
+
+        <div className="p-4 mb-4 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800"
+             role="alert">
+          <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
+        </div>
+        <div className="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800"
+             role="alert">
+          <span className="font-medium">Danger alert!</span> Change a few things up and try submitting again.
+        </div>
+        <div className="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800"
+             role="alert">
+          <span className="font-medium">Success alert!</span> Change a few things up and try submitting again.
+        </div>
+        <div
+            className="p-4 mb-4 text-sm text-yellow-700 bg-yellow-100 rounded-lg dark:bg-yellow-200 dark:text-yellow-800"
+            role="alert">
+          <span className="font-medium">Warning alert!</span> Change a few things up and try submitting again.
+        </div>
+        <div className="p-4 text-sm text-gray-700 bg-gray-100 rounded-lg dark:bg-gray-700 dark:text-gray-300"
+             role="alert">
+          <span className="font-medium">Dark alert!</span> Change a few things up and try submitting again.
+        </div>
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js 13!</a>
         </h1>
